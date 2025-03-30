e88fcd4e49be5d959d618619797300024af8cf79
Soujash-123 <72877997+Soujash-123@users.noreply.github.com>
2025-03-30 23:04:51 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -201,4 +201,4 @@ def razorpay_webhook():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True,port="5001")
+    app.run(debug=True,host="0.0.0.0",port="5001")
